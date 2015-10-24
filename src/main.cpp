851625656144b43d97194f0616348f0c641d5c3a
Tino Rusch <tino.rusch@webvariants.de>
2015-10-24 18:29:07 +0200

    make main.cpp (the usage example) more verbose;

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,18 @@
 
 int main(){
 	BCrypt bcrypt;
-	std::string password = "test";
-	std::string hash = bcrypt.generateHash(password);
-	std::cout<<bcrypt.validatePassword(password,hash)<<std::endl;
-	std::cout<<bcrypt.validatePassword("test1",hash)<<std::endl;
+	std::string right_password = "right_password";
+	std::string wrong_password = "wrong_password";
+
+	std::cout << "generate hash... " << std::flush;
+	std::string hash = bcrypt.generateHash(right_password);
+	std::cout << "done." << std::endl;
+
+	std::cout << "checking right password: " << std::flush
+			  << bcrypt.validatePassword(right_password,hash) << std::endl;
+
+	std::cout << "checking wrong password: " << std::flush
+			  << bcrypt.validatePassword(wrong_password,hash) << std::endl;
+
 	return 0;
 }
