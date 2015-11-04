f9b33f9ee8871c1c14c042ba2082a460acf8b508
Tino Rusch <tino.rusch@webvariants.de>
2015-11-04 14:26:07 +0100

    make interface more simple due to the usage of static functions;

--- include/bcrypt/BCrypt.hpp
+++ include/bcrypt/BCrypt.hpp
@@ -6,23 +6,19 @@
 #include <stdexcept>
 
 class BCrypt {
-protected:
-    int _workFactor;
 public:
-    BCrypt(int workFactor = 12) : _workFactor{workFactor} {}
-
-    std::string generateHash(const std::string & password){
+    static std::string generateHash(const std::string & password, int workload = 12){
         char salt[BCRYPT_HASHSIZE];
         char hash[BCRYPT_HASHSIZE];
         int ret;
-        ret = bcrypt_gensalt(12, salt);
+        ret = bcrypt_gensalt(workload, salt);
         if(ret != 0)throw std::runtime_error{"bcrypt: can not generate salt"};
         ret = bcrypt_hashpw(password.c_str(), salt, hash);
         if(ret != 0)throw std::runtime_error{"bcrypt: can not generate hash"};
         return std::string{hash};
     }
 
-    bool validatePassword(const std::string & password, const std::string & hash){
+    static bool validatePassword(const std::string & password, const std::string & hash){
         return (bcrypt_checkpw(password.c_str(), hash.c_str()) == 0);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,18 @@
 #include <iostream>
 
 int main(){
-	BCrypt bcrypt;
 	std::string right_password = "right_password";
 	std::string wrong_password = "wrong_password";
 
 	std::cout << "generate hash... " << std::flush;
-	std::string hash = bcrypt.generateHash(right_password);
+	std::string hash = BCrypt::generateHash(right_password, 12);
 	std::cout << "done." << std::endl;
 
 	std::cout << "checking right password: " << std::flush
-			  << bcrypt.validatePassword(right_password,hash) << std::endl;
+			  << BCrypt::validatePassword(right_password,hash) << std::endl;
 
 	std::cout << "checking wrong password: " << std::flush
-			  << bcrypt.validatePassword(wrong_password,hash) << std::endl;
+			  << BCrypt::validatePassword(wrong_password,hash) << std::endl;
 
 	return 0;
 }
